6a14e52c913e06e49a0a5a67cd2660dad05a8a45
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-10-07 22:25:26 -0400

    added links for bootstrap/jquery, added 2nd section including 5 divs with h1/ul/li elements for 5 day forecast, added ids to all li and h1 tags inside 2nd section, added variables in JS, used those variables with .text to add information from weather API to display on webpage, deleted some consolelogs

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,16 @@
 console.log("======");
 
+var selectedCity = $("#selected-city");
+var currentTemp = $("#current-temp");
+var currentWind = $("#current-wind");
+var currentHumid = $("#current-humid");
+var currentUv = $("#current-uv");
+
 var searchButton = document.getElementById("search-button");
 var weatherContainer = document.getElementById("weather-display");
-var cityInput = document.getElementById("city");
-var currentDate = moment();
+// var cityInput = document.getElementById("city");
+// var cityInput = $('#city').val();
+var currentDay = moment();
 
 function displayWeather() {
   var currentConditions = `https://api.openweathermap.org/data/2.5/weather?q=philadelphia&appid=c3b781a7de141c8e06094287b67539ce`;
@@ -15,14 +22,16 @@ function displayWeather() {
       return response.json();
     })
     .then(function (data) {
-    //   console.log(data);
+      console.log(data);
       console.log(data.name);
-      console.log(currentDate.format("L"));
+      var currentDate = currentDay.format("L");
       console.log(data.weather[0].icon);
       var fahrenheit = Math.floor((data.main.temp - 273.15) * (9 / 5) + 32);
-      console.log(fahrenheit);
-      console.log(data.wind.speed);
-      console.log(data.main.humidity);
+
+      selectedCity.text(`${data.name} (${currentDate}) ${data.weather[0].icon}`);
+      currentTemp.text(`Temp: ${fahrenheit}° F`);
+      currentWind.text(`Wind: ${data.wind.speed} MPH`);
+      currentHumid.text(`Humidity: ${data.main.humidity} %`);
 
       var lat = data.coord.lat;
       var lon = data.coord.lon;
@@ -33,34 +42,37 @@ function displayWeather() {
           return response.json();
         })
         .then(function (data) {
-        //   console.log(data);
+          //   console.log(data);
           console.log(data.current.uvi);
+          currentUv.text(`UV Index: ${data.current.uvi}`);
 
           for (var i = 1; i < 6; i++) {
             // console.log(data);
+            var date5 = $("#date-" + i);
+            var icon5 = $("#icon-" + i);
+            var temp5 = $("#temp-" + i);
+            var wind5 = $("#wind-" + i);
+            var humid5 = $("#humid-" + i);
             var date = new Date(data.daily[i].dt * 1000);
-            console.log(date);
-            console.log(data.daily[i].weather[0].icon)
-            var dailyFahrenheit = Math.floor((data.daily[i].temp.day - 273.15) * (9 / 5) + 32);
-            console.log(dailyFahrenheit)
-            console.log(data.daily[i].wind_speed)
-            console.log(data.daily[i].humidity)
+            var dailyFahrenheit = Math.floor(
+              (data.daily[i].temp.day - 273.15) * (9 / 5) + 32
+            );
 
+            date5.text(date);
+            icon5.text(data.daily[i].weather[0].icon);
+            temp5.text(`Temp: ${dailyFahrenheit}° F`);
+            wind5.text(`Wind: ${data.daily[i].wind_speed} MPH`);
+            humid5.text(`Humidity: ${data.daily[i].humidity} %`);
           }
           //closes for loop
         });
-        //closes 2nd .then(function 
+      //closes 2nd .then(function
     });
-    //closes 1st .then(function 
+  //closes 1st .then(function
 }
 //closes displayweather function
 
 displayWeather();
-
-
-
-
-
 
 // Events needed for
 
