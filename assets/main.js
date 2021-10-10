0e2329739e0453adb910dde7d3e6e52bf8d47f23
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-10-10 18:14:49 -0400

    formatted js document, added if statement to display an alert if the responsestatus was 404, still need to work on that so it doesn't added it to li

--- assets/main.js
+++ assets/main.js
@@ -5,37 +5,39 @@ var currentTemp = $("#current-temp");
 var currentWind = $("#current-wind");
 var currentHumid = $("#current-humid");
 var currentUv = $("#current-uv");
-var weatherContainer = $('.weather-container');
-var searchButton = $('#search-button');
-var listOfCities =$('.city-searched');
+var weatherContainer = $(".weather-container");
+var searchButton = $("#search-button");
+var listOfCities = $(".city-searched");
 var currentDay = moment();
 
-var city = []
+var city = [];
 if (localStorage.getItem("city") != null) {
   city = JSON.parse(localStorage.getItem("city"));
 }
 function saveCity() {
   localStorage.setItem("city", JSON.stringify(city));
 }
-// will use push for the array rather than index, adding not replacing
 
 function initial() {
-  weatherContainer.attr("style", "display: none")
+  weatherContainer.attr("style", "display: none");
 
-searchButton.on('click', function(){
-  displayWeather();
-  weatherContainer.attr("style", "display: block")
+  searchButton.on("click", function () {
+    displayWeather();
+    weatherContainer.attr("style", "display: block");
 
-  var li = $('<li>');
+    var li = $("<li>");
     // li.attr("data-index", i);
     listOfCities.append(li);
-    var cityInput = $('#city').val();
-    console.log(typeof cityInput)
+    var cityInput = $("#city").val();
     li.text(cityInput.toUpperCase());
 
-  city.push(cityInput)
-  saveCity();
-});
+    city.push(cityInput);
+
+    //enter function for checking if city was previously stated
+    // use for loop for said function to go through array and check if input from search == city[i] and if yes then stop, if not then goes through
+
+    saveCity();
+  });
 }
 // closes initial function
 
@@ -50,26 +52,27 @@ searchButton.on('click', function(){
 //     li.setAttribute("data-index", i);
 //     document.querySelector("#answer").appendChild(li);
 //     li.textContent = `${highscoreInitial} ${highScore}`;
-    
+
 //   };
 
 function displayWeather() {
-  var cityInput = $('#city').val();
-  console.log(cityInput)
+  var cityInput = $("#city").val();
   var currentConditions = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=5b787b122cd0fc16a703d189885623e5`;
-  // var fiveDayForecast = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}'
-  // links to weather api's
 
   fetch(currentConditions)
     .then(function (response) {
+      // console.log(response.status);
+      if (response.status === 404) {
+        alert("Error: City not found, please try again.")
+        initial();
+      }
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       var currentDate = currentDay.format("L");
       var iconcode = data.weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-      $('#wicon').attr('src', iconurl);
+      $("#wicon").attr("src", iconurl);
 
       selectedCity.text(`${data.name} (${currentDate})`);
       currentTemp.text(`Temp: ${data.main.temp}° F`);
@@ -85,24 +88,22 @@ function displayWeather() {
           return response.json();
         })
         .then(function (data) {
-            console.log(data);
-          console.log(data.current.uvi);
           currentUv.text(`UV Index: ${data.current.uvi}`);
 
           for (var i = 1; i < 6; i++) {
-            // console.log(data);
             var date5 = $("#date-" + i);
             var temp5 = $("#temp-" + i);
             var wind5 = $("#wind-" + i);
             var humid5 = $("#humid-" + i);
-            var wicon5 =$("#wicon-" + i)
+            var wicon5 = $("#wicon-" + i);
             var futurDate = new Date(data.daily[i].dt * 1000);
-            var date = moment(futurDate).format('MM/DD/YYYY');
+            var date = moment(futurDate).format("MM/DD/YYYY");
             var iconcode = data.daily[i].weather[0].icon;
-            var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            var iconurl =
+              "https://openweathermap.org/img/w/" + iconcode + ".png";
 
             date5.text(date);
-            wicon5.attr('src', iconurl);
+            wicon5.attr("src", iconurl);
             temp5.text(`Temp: ${data.daily[i].temp.day}° F`);
             wind5.text(`Wind: ${data.daily[i].wind_speed} MPH`);
             humid5.text(`Humidity: ${data.daily[i].humidity} %`);
@@ -116,8 +117,6 @@ function displayWeather() {
 //closes displayweather function
 
 initial();
-
-
 
 // Events needed for
 
